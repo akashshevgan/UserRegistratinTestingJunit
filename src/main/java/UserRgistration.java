fc682c6ef3bd32165a3f4d05b2aa781ca309bda0
akash <akash@MSI>
2021-05-06 22:54:14 +0530

    create all test cases

--- src/main/java/UserRgistration.java
+++ src/main/java/UserRgistration.java
@@ -1,24 +1,54 @@
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRgistration {
-    boolean test;
-    public boolean firstNameTest() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the first name");
-        String string = scan.nextLine();
-        test= Pattern.compile("(^[A-Z]{1}[a-z]{2,}$)").matcher(string).matches();
 
-        if (test){
+    public boolean firstNameTest(String firstname) {
+        boolean test = Pattern.compile("(^[A-Z]{1}[a-z]{2,}$)").matcher(firstname).matches();
+        if (test) {
             System.out.println("First name is valid");
-        }
-        else {
-            System.out.println("First name is not valid");
+        } else {
+            System.out.println("First name is invalid");
         }
         return test;
     }
-    public static void main(String[] args) {
-        UserRgistration userRgistration = new UserRgistration();
-        userRgistration.firstNameTest();
+
+    public boolean lastNameTest(String lastname) {
+        boolean test = Pattern.compile("^[A-Z][a-z]{3,20}").matcher(lastname).matches();
+        if (test) {
+            System.out.println(lastname + " lastname is Valid ");
+        } else {
+            System.out.println(lastname + " lastname is Invalid ");
+        }
+        return test;
+    }
+
+    public boolean emailTest(String email) {
+        boolean test = Pattern.compile("(^[a-z 0-9]{0,30} *([.+/-]?[a-z 0-9]{1,20})@[a-z]{1,20}.([a-z]{2,3} *(.[a-z]{2,3}))$)").matcher(email).matches();
+        if (test) {
+            System.out.println(email + " email is Valid ");
+        } else {
+            System.out.println(email + " email is Invalid ");
+        }
+        return test;
+    }
+
+    public boolean mobileNumberTest(String phone) {
+        boolean test = Pattern.compile("^[0-9]{2}\\s[0-9]{10}").matcher(phone).matches();
+        if (test) {
+            System.out.println(phone + " mobile number is Valid ");
+        } else {
+            System.out.println(phone + " mobile number is Invalid ");
+        }
+        return test;
+    }
+
+    public boolean  passwordTest (String password) {
+        boolean test = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]?){8,32}.*$").matcher(password).matches();
+        if (test) {
+            System.out.println (password + " password is Valid ");
+        } else {
+            System.out.println (password + " password is Invalid ");
+        }
+        return test;
     }
 }
--- src/test/java/TestUserRegistrain.java
+++ src/test/java/TestUserRegistrain.java
@@ -2,11 +2,38 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestUserRegistrain {
+
     @Test
-    public void firstNameTest() {
+    public void firstName() {
         UserRgistration firstName = new UserRgistration();
-        boolean result = firstName.firstNameTest();
-        Assertions.assertEquals(false,result);
+        boolean result = firstName.firstNameTest("Akash");
+        Assertions.assertEquals(true,result);
+    }
+
+    @Test
+    public void lastName() {
+        UserRgistration lastName = new UserRgistration();
+        boolean result = lastName.lastNameTest("Shevgan");
+        Assertions.assertEquals(true,result);
+    }
+
+    @Test
+    public void email() {
+        UserRgistration user = new UserRgistration();
+        boolean result = user.emailTest("akashshevgan11@gmail.com");
+        Assertions.assertEquals(true,result);
+    }
+
+    @Test
+    public void phone() {
+        UserRgistration user = new UserRgistration();
+        boolean result = user.mobileNumberTest("91 8793447817");
+        Assertions.assertEquals(true,result);
+    }
+    @Test
+    public void password() {
+        UserRgistration user = new UserRgistration();
+        boolean result = user.passwordTest("Akash123$");
+        Assertions.assertEquals(true,result);
     }
 }
-
